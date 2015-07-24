7a24618b58d811bb762f1868db9105cc3870c356
Kenneth Ford <kenneth.ford93@gmail.com>
2015-07-23 21:24:35 -0400

    First Commit

---
+++ src/palindrome.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.lang.reflect.Array;
+
+/**
+ * Created by kennethford on 7/23/15.
+ */
+public class palindrome {
+    String pathToOpen;
+    String lineToRead = "";
+    String cleanString;
+    int numOfWords;
+    int numOfPalin;
+    boolean isPassagePalin;
+    String finalString="";
+
+    public palindrome(String path) {
+        pathToOpen = path;
+        try {
+            // open input stream test.txt for reading purpose.
+            BufferedReader br = new BufferedReader(new FileReader(new File(pathToOpen)));
+            while ((lineToRead = br.readLine()) != null) {
+                cleanString = lineToRead.replaceAll("[^A-Za-z ]+", "").toLowerCase();
+                String[] words = cleanString.split(" ");
+                numOfWords = words.length;
+                findPalindromes(words);
+                System.out.println(finalString);
+                System.out.println(numOfPalin + " " + numOfWords + " " + isPassagePalin);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void findPalindromes(String[] arrayToCheck){
+        for(int i = 0; i<arrayToCheck.length; i++){
+            if(detectPalindrome(arrayToCheck[i])){
+                numOfPalin++;
+            }
+            finalString += arrayToCheck[i];
+        }
+        isPassagePalin = detectPalindrome(finalString);
+    }
+
+    boolean detectPalindrome(String wordToCheck) {
+        int length = wordToCheck.length();
+        for (int j = 0; j < length / 2; j++) {
+            if (wordToCheck.charAt(j) == wordToCheck.charAt(length - 1 - j)) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/palindromeControl.java
@@ -0,0 +1,9 @@
+/**
+ * Created by kennethford on 7/23/15.
+ */
+public class palindromeControl {
+    public static void main(String []args) {
+        palindrome pal = new palindrome("/Users/kennethford/Documents/palindrome/test.txt");
+    }
+}
+
